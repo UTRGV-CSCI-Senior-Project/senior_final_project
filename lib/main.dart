f4d8f6f1b43376b47e977fb1683e40a2a89b27f3
Ana Garcia <anagarcia@Anas-Air.home>
2024-09-20 21:20:32 -0500

    Firebase integration finished

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'views/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
