352c27aa756e42ec0b97e82ed3f4ae4c40342658
anagarcia3174 <anagarcia3174@gmail.com>
2024-10-08 07:27:37 -0500

    Changed name in UI to Folio

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -13,7 +13,7 @@ class LoadingScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "APP NAME",
+              "Folio",
               style: TextStyle(
                 fontWeight: FontWeight.w400,
                 fontSize: 48.0,
