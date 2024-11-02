23d14cf84281b5f6f8287b06e081ba3d663b3c57
carldlanor23 <carldlanor.fornal01@utrgv.edu>
2024-11-01 22:16:33 -0500

    facebook configuration

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -2,8 +2,6 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\carla\Documents\FlutterProjects\folio"
-export "FLUTTER_ROOT=C:\Dart\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\mendo\dartCode\senior_final_project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
