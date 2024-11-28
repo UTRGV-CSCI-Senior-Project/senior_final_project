56275777bb66705daba550fa554f6215785dffbd
Ana Garcia <anagarcia3174@gmail.com>
2024-11-27 22:23:54 -0600

    Added false to emulator set up for integration tests

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -178,6 +178,7 @@ void setupEmulators({bool useEmulators = false}) {
       FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
       FirebaseStorage.instance.useStorageEmulator('127.0.0.1', 9199);
       FirebaseFunctions.instance.useFunctionsEmulator('127.0.0.1', 5001);
+      FirebaseMessaging.instance.setAutoInitEnabled(false);
       // Add other emulators as needed
 
       developer.log(
